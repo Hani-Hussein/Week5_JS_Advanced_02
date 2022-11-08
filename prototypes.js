0467113a3bd337ee585baf70ebcd2f861a5dcb85
Hani Hussein <hanihussein@Hanis-MacBook.local>
2022-11-07 23:38:25 -0700

    making changes to git hub

--- prototypes.js
+++ prototypes.js
@@ -1,12 +1,37 @@
-// In this assignment, you will be creating a a constructor function named "Person" and then create your whole family from that constructor.
+// In this assignment, you will be creating a constructor function named "Person" and then create your whole family from that constructor.
 
 
 // Create "Person" constructor function
 // Give it "Name", "Age", "Hobby" properties
 // Also give it a method "Speak", which says "Hello, My name is + name + and I'm + age + years old. I love + Hobby"
 
+function Person(attributes){
+    this.age = attributes.age;
+    this.name = attributes.name;
+    this.hobby = attributes.hobby;
+    // this.speak = functon(){
+    //     return `"Hello, My name is + name + and I'm + age + years old. I love + Hobby"`;
+    };
+    
+    Person.prototype.speak = function() {
+    
+        return `Hello, My name is ${this.name} and I'm ${this.age} years old. I love to ${this.hobby} `;
+    };
+    
+    
+    
+    
+     const me = new Person({
+      name: "Hani",
+      age:  26,
+      hobby: "travel",
+    
+     })
+    
+    
+     console.log(me.speak());
 
 // Move the "Speak" method outside of the constructor function and into the prototype.
 // Create an object named "Me" using the "Person" constructor function
 // Call the "Speak" method on the "Me" object
-// Console log all your results
+// Console log all your resh
